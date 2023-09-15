e0bc05b50a5f6916fe18c0f064a820b8cb498442
prasannaballa <prasannab1769@gmail.com>
2023-09-15 20:46:44 +0530

    update

--- power.ps1
+++ power.ps1
@@ -1,3 +1,22 @@
+ # Here we are defining the application object properties
+# Remember to change the AppId, AppSecret and TenantID values accordingly.
+
+
+# Authenticate to Azure
+Connect-AzAccount
+
+if ($action -eq "start") {
+    Start-AzWebApp -ResourceGroupName $resourceGroupName -Name $webAppName
+    Write-Output "Azure App Service '$webAppName' started."
+}
+elseif ($action -eq "stop") {
+    Stop-AzWebApp -ResourceGroupName $resourceGroupName -Name $webAppName
+    Write-Output "Azure App Service '$webAppName' stopped."
+}
+else {
+    Write-Output "Invalid action. Use 'start' or 'stop'."
+}
+
  # Run the PowerShell script to start or stop the Azure App Service
-    .\StartStopAppService.ps1 -action "start" -resourceGroupName "node" -webAppName "nodeapp23"
-  displayName: 'Run PowerShell script'
+
+  
