aba695abc8b7c955f69fe1fc8a5ac416dbd17a21
prasannaballa <prasannab1769@gmail.com>
2023-09-15 20:27:59 +0530

    update

---
+++ power.ps1
@@ -0,0 +1,3 @@
+ # Run the PowerShell script to start or stop the Azure App Service
+    .\StartStopAppService.ps1 -action "start" -resourceGroupName "node" -webAppName "nodeapp23"
+  displayName: 'Run PowerShell script'
